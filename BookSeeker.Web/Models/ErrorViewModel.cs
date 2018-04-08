4008aec0870e6ff945638397bb78cd4bbbeb50b6
Damian Sulewski <sulewskidamian@gmail.com>
2018-04-08 11:04:39 +0200

    Add project files.

---
+++ BookSeeker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookSeeker.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
